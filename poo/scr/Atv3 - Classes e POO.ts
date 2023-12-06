646dd9a9c27ba9af0633316da4e5f734352870f2
JadsonMaciel <jdsm.eai20@uea.edu.br>
2023-12-06 18:28:25 -0400

    modificação atividade poo

--- poo/scr/Atv3 - Classes e POO.ts
+++ poo/scr/Atv3 - Classes e POO.ts
@@ -1,51 +1,82 @@
 // Classe base
 class Entidade {
-  constructor(public nome: string) {}
+  private _nome: string;
+
+  constructor(nome: string) {
+    this._nome = nome;
+  }
+
+  
+  get nome(): string {
+    return this._nome;
+  }
+
+  
+  getnome()  {
+    return this._nome;
+  }
+  
+  setnome(nome: string){
+    this._nome = nome;
+  }
+
+  
+  apresentar(): void {
+    console.log(`Olá, eu sou ${this.nome}.`);
+  }
 }
 
 // Classe 1 - Pessoa
 class Pessoa extends Entidade {
-  idade: number;
-  cidade: string;
+  private _idade: number;
+  private _cidade: string;
 
   constructor(nome: string, idade: number, cidade: string) {
     super(nome);
-    this.idade = idade;
-    this.cidade = cidade;
+    this._idade = idade;
+    this._cidade = cidade;
   }
 
+  
   apresentar(): void {
-    console.log(`Olá, eu sou ${this.nome}, tenho ${this.idade} anos e moro em ${this.cidade}.`);
+    console.log(`Olá, eu sou ${this.nome}, tenho ${this._idade} anos e moro em ${this._cidade}.`);
   }
 }
 
 // Classe 2 - Produto
 class Produto extends Entidade {
-  preco: number;
-  categoria: string;
+  private _preco: number;
+  private _categoria: string;
 
   constructor(nome: string, preco: number, categoria: string) {
     super(nome);
-    this.preco = preco;
-    this.categoria = categoria;
+    this._preco = preco;
+    this._categoria = categoria;
   }
 
-  exibirDetalhes(): void {
-    console.log(`Produto: ${this.nome}, Preço: R$${this.preco.toFixed(2)}, Categoria: ${this.categoria}.`);
+ 
+  apresentar(): void {
+    console.log(`Produto: ${this.nome}, Preço: R$${this._preco.toFixed(2)}, Categoria: ${this._categoria}.`);
   }
 }
 
 // Classe 3 - Carro
 class Carro extends Entidade {
-  ano: number;
-  cor: string;
+  private _ano: number;
+  private _cor: string;
 
   constructor(nome: string, ano: number, cor: string) {
     super(nome);
-    this.ano = ano;
-    this.cor = cor;
+    this._ano = ano;
+    this._cor = cor;
   }
 
+  
+  apresentar(): void {
+    console.log(`${this.nome} é um carro fabricado em ${this._ano} e tem a cor ${this._cor}.`);
+  }
+
+  
   ligar(): void {
     console.log(`${this.nome} está ligado.`);
   }
@@ -53,18 +84,12 @@ class Carro extends Entidade {
 
 // Criar objetos para cada classe
 const pessoa1 = new Pessoa("João", 25, "São Paulo");
-const pessoa2 = new Pessoa("Maria", 30, "Rio de Janeiro");
-const pessoa3 = new Pessoa("Carlos", 22, "Belo Horizonte");
-
-const produto1 = new Produto("Celular", 1200, "Eletrônicos");
 const produto2 = new Produto("Livro", 30, "Cultura");
-const produto3 = new Produto("Notebook", 2500, "Eletrônicos");
-
-const carro1 = new Carro("Civic", 2022, "Prata");
-const carro2 = new Carro("Gol", 2019, "Azul");
 const carro3 = new Carro("Corolla", 2020, "Preto");
 
-// Interagindo com os objetos
-pessoa1.apresentar();
-produto2.exibirDetalhes();
-carro3.ligar();
+// Interagindo com os objetos usando polimorfismo
+const entidades: Entidade[] = [pessoa1, produto2, carro3];
+
+for (const entidade of entidades) {
+  entidade.apresentar();
+}
